6b6b3870dfdf73970052c362ddcd4e392848b993
Jure Cuhalev <jure@cuhalev.com>
2019-01-01 13:40:15 +0100

    Mobile style for heading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,14 +68,14 @@
     @endcomponent
   </div>
 
-  <h2 class="heading-lg w-2/3 md:w-1/3">About</h2>
-  <div class="flex flex-wrap -mx-4 overflow-hidden mb-26">
+  <h2 class="heading w-2/3 md:w-1/3">About</h2>
+  <div class="flex flex-wrap -mx-4 overflow-hidden mb-20 md:mb-26">
     <div class="my-2 px-4 w-1/3 overflow-hidden text-center hidden md:block">
       @svg('logo', 'max-w-full')
     </div>
 
     <div class="my-2 px-4 md:w-7/12 overflow-hidden">
-      <p class="mb-6">The Open Education Global Conference is where the world meets to discuss how opening education helps us achieve
+      <p class="mb-6 sm-only:text-sm">The Open Education Global Conference is where the world meets to discuss how opening education helps us achieve
         universal access, equity, innovation and opportunity in education. The OE Global conference is the most
         internationally diverse conference devoted exclusively to open education, attracting researchers, practitioners,
         policy makers, educators and students from more than 35 countries to discuss and explore how Open Education
@@ -84,14 +84,14 @@
     </div>
   </div>
 
-  <h2 class="heading-lg w-2/3 md:w-1/3">The Venue</h2>
+  <h2 class="heading w-2/3 md:w-1/3">The Venue</h2>
   <div class="flex flex-wrap -mx-4 overflow-hidden mb-26">
     <div class="my-2 px-4 w-1/3 overflow-hidden text-right hidden md:block">
       @svg('symbols/venue')
     </div>
 
     <div class="my-2 px-4 md:w-7/12 overflow-hidden">
-      <p>The Open Education Global Conference is where the world meets to discuss how opening education helps us achieve
+      <p class="mb-6 sm-only:text-sm">The Open Education Global Conference is where the world meets to discuss how opening education helps us achieve
         universal access, equity, innovation and opportunity in education. The OE Global conference is the most
         internationally diverse conference devoted exclusively to open education, attracting researchers, practitioners,
         policy makers, educators and students from more than 35 countries to discuss and explore how Open Education
