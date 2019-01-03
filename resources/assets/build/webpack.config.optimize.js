e80b856d2e7b91b317aed4917bd2c57fc33fb325
Jure Cuhalev <jure@cuhalev.com>
2019-01-03 10:53:19 +0100

    Adds purgecss

--- resources/assets/build/webpack.config.optimize.js
+++ resources/assets/build/webpack.config.optimize.js
@@ -1,25 +1,34 @@
 'use strict'; // eslint-disable-line
 
-const { default: ImageminPlugin } = require('imagemin-webpack-plugin');
+const {default: ImageminPlugin} = require('imagemin-webpack-plugin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob-all');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const whitelister = require('purgecss-whitelister');
 
 const config = require('./config');
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 module.exports = {
   plugins: [
     new ImageminPlugin({
-      optipng: { optimizationLevel: 7 },
-      gifsicle: { optimizationLevel: 3 },
-      pngquant: { quality: '65-90', speed: 4 },
+      optipng: {optimizationLevel: 7},
+      gifsicle: {optimizationLevel: 3},
+      pngquant: {quality: '65-90', speed: 4},
       svgo: {
         plugins: [
-          { removeUnknownsAndDefaults: false },
-          { cleanupIDs: false },
-          { removeViewBox: false },
+          {removeUnknownsAndDefaults: false},
+          {cleanupIDs: false},
+          {removeViewBox: false},
         ],
       },
-      plugins: [imageminMozjpeg({ quality: 75 })],
+      plugins: [imageminMozjpeg({quality: 75})],
       disable: (config.enabled.watcher),
     }),
     new UglifyJsPlugin({
@@ -30,6 +39,22 @@ module.exports = {
           drop_console: true,
         },
       },
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync([
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'resources/assets/scripts/**/*.js',
+        'node_modules/lightgallery/dist/js/*.js',
+        'node_modules/lightgallery/dist/css/*.css',
+      ]),
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['html', 'js', 'php'],
+        },
+      ],
+      whitelist: [],
     }),
   ],
 };
--- resources/views/components/panel-content.blade.php
+++ resources/views/components/panel-content.blade.php
@@ -1,7 +1,9 @@
 <div class="flex flex-wrap -mx-4 overflow-hidden mb-20 md:mb-26">
+  @if ($svg)
   <div class="my-2 px-4 w-1/3 overflow-hidden hidden md:block {{ $image_classes }}">
     @svg($svg, $svg_classes)
   </div>
+  @endif
 
   <div class="my-2 px-4 md:w-7/12 overflow-hidden panel">
     {{ $slot }}
