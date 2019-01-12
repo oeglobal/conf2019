820eaed2ac3d54e4d6b60b9568df63906b395cc7
Jure Cuhalev <jure@cuhalev.com>
2019-01-12 21:44:05 +0100

    Update hero

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,11 +1,11 @@
 <div class="hero flex items-center justify-center">
-  <div class="text-white hero-text text-center">
-    <h1>Open Future <br/>for an Open Education</h1>
-    <div class="tagline">Resources, Practices, Communities</div>
+  <div class="text-white hero-text text-center flex flex-col justify-center items-center">
+    <h1>Open Education <br class="md:hidden"/> Global Conference</h1>
+    <div class="tagline">Open Future for an Open Education</div>
 
-    <div class="name">Open Education Global Conference</div>
+    <div class="name">Resources, Practices, Communities</div>
     <div class="location-date flex justify-between flex-col md:flex-row mb-4 md:mb-14">
-      <span class="block md:inline mb-4 md:mb-0">
+      <span class="block md:inline mb-4 md:mb-0 md:mr-10">
         <span class="block md:inline">@svg('icon-pin')</span>
         <span class="block md:inline">Politecnico di Milano</span>
       </span>
