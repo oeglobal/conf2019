0777a2e9325129dfe632c0bd4409da13828c57e8
Jure Cuhalev <jure@cuhalev.com>
2019-01-13 10:19:32 +0100

    Support attribution images without links

--- resources/views/partials/gallery.blade.php
+++ resources/views/partials/gallery.blade.php
@@ -3,7 +3,11 @@
     <a
       href="{!! $image['image']['url'] !!}"
       class="w-2/3 md:w-1/3 inline-block gallery-item"
-      data-sub-html="{!! $image['caption'] !!} (<a href='{!! $image['attribution_link'] !!}'>{!! $image['attribution_name'] !!}</a>)"
+      @if ($image['attribution_link'])
+        data-sub-html="{!! $image['caption'] !!} (<a href='{!! $image['attribution_link'] !!}'>{!! $image['attribution_name'] !!}</a>)"
+      @elseif ($image['attribution_name'])
+        data-sub-html="{!! $image['caption'] !!} ({!! $image['attribution_name'] !!})"
+      @endif
       data-exthumbimage="{!! $image['image']['sizes']['thumbnail'] !!}"
     >
       <span
