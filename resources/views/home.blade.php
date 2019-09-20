2778588ab7c88458be9731370ebd806ce7da8bb6
Jure Cuhalev <jure@cuhalev.com>
2019-09-20 13:11:34 +0200

    Add new icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,6 @@
 @section('content')
   <div class="justify-around mb-10 hidden md:flex">
     @component('components.symbol', [
-      'svg' => 'welcome-active',
-      'text' => 'Welcome',
-      'size' => 'sm',
-      'class' => 'w-32',
-      'link' => home_url('/welcome/')
-    ])
-    @endcomponent
-
-    @component('components.symbol', [
       'svg' => 'venue-active',
       'text' => 'The Venue',
       'size' => 'sm',
@@ -25,11 +16,20 @@
     @endcomponent
 
     @component('components.symbol', [
-      'svg' => 'cfp-active',
-      'text' => 'Call for Proposals',
+      'svg' => 'program-active',
+      'text' => 'Schedule',
       'size' => 'sm',
       'class' => 'w-32',
-      'link' => home_url('/cfp/'),
+      'link' => home_url('/sched-app/')
+    ])
+    @endcomponent
+
+    @component('components.symbol', [
+      'svg' => 'sched-active',
+      'text' => 'Mobile Schedule',
+      'size' => 'sm',
+      'class' => 'w-32',
+      'link' => home_url('/sched-app/')
     ])
     @endcomponent
 
@@ -73,20 +73,26 @@
     'svg_classes' => 'max-w-full w-32',
     'link' => home_url('/venue/')
   ])
-    <p>The OE Global 2019 will be hosted at the Milano Bovisa Campus, north of Milan. The campus has been substantially expanded as the result of an international competition, which was announced in 1998 by Politecnico di Milano in collaboration with other local entities (Municipality and Region) and which has brought about a general renewal of the zone.</p>
+    <p>The OE Global 2019 will be hosted at the Milano Bovisa Campus, north of Milan. The campus has been substantially
+      expanded as the result of an international competition, which was announced in 1998 by Politecnico di Milano in
+      collaboration with other local entities (Municipality and Region) and which has brought about a general renewal of
+      the zone.</p>
   @endcomponent
 
   @include('partials.gallery')
 
   @include('components.panel-heading', ['title' => 'Organized by'])
   <div class="flex flex-wrap items-center justify-center pt-8 mb-32">
-    <a href="https://www.polimi.it/"><img src="@asset('images/logo-politecnico.svg')" alt="Politecnico di Milano" class="max-w-full h-16 mb-10 md:mb-0 md:mr-10 md:h-22" /></a>
-    <a href="https://www.oeconsortium.org/"><img src="@asset('images/logo-oec.svg')" alt="Open Education Consortium" class="max-w-full h-16 mb-10 md:mb-0 md:mr-10 md:h-22" /></a>
+    <a href="https://www.polimi.it/"><img src="@asset('images/logo-politecnico.svg')" alt="Politecnico di Milano"
+                                          class="max-w-full h-16 mb-10 md:mb-0 md:mr-10 md:h-22"/></a>
+    <a href="https://www.oeconsortium.org/"><img src="@asset('images/logo-oec.svg')" alt="Open Education Consortium"
+                                                 class="max-w-full h-16 mb-10 md:mb-0 md:mr-10 md:h-22"/></a>
   </div>
 
   @include('components.panel-heading', ['title' => 'Collaborators'])
   <div class="flex flex-wrap items-center justify-center pt-8 mb-32">
-    <a href="https://openpraxis.org"><img src="@asset('images/logo-open-praxis.svg')" alt="Open Praxis" class="max-w-full h-16 md:h-22" /></a>
+    <a href="https://openpraxis.org"><img src="@asset('images/logo-open-praxis.svg')" alt="Open Praxis"
+                                          class="max-w-full h-16 md:h-22"/></a>
   </div>
 
   {{--
