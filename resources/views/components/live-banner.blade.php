2c931bcc95f7e24a7881c11c1df09a55c9143620
Jure Cuhalev <jure@cuhalev.com>
2019-11-18 18:27:27 +0100

    Fixes livestream link

--- resources/views/components/live-banner.blade.php
+++ resources/views/components/live-banner.blade.php
@@ -1,5 +1,5 @@
 <div class="live-banner text-center h-16 flex justify-center items-center">
-  <a class="block font-brand" href="/live/">
+  <a class="block font-brand" href="home_url('/live/')">
     Live streaming - Watch @svg('icon-arrow-right', 'icon-arrow')
   </a>
 </div>
