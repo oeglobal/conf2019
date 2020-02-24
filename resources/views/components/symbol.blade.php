5af4d2a697de330fbeb27699f04c0a4905499885
Jure Cuhalev <jure@cuhalev.com>
2020-02-24 15:08:22 +0100

    Update hero branding

--- resources/views/components/symbol.blade.php
+++ resources/views/components/symbol.blade.php
@@ -1,4 +1,4 @@
-{{-- symbol-sm --}}
+{{-- symbol-sm symbol.active active --}}
 <a class="inline-block symbol text-center {!! $svg !!} {!! 'symbol-' . $size !!} {!! $class !!}"
    href="{!! $link !!}">
   @svg("symbols/$svg")
