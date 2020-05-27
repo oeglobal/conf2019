35ddcf11909f4b36b74f7393ce7f2259d4754d23
Jure Cuhalev <jure@cuhalev.com>
2020-05-27 17:11:11 +0200

    Right align share on home page

--- resources/views/partials/share.blade.php
+++ resources/views/partials/share.blade.php
@@ -1,5 +1,7 @@
-<div class="left-0 hidden md:block sticky social h-none relative">
-  <div class="absolute bg-white px-2 w-10 text-center">
+<div class="hidden md:block sticky social h-none relative">
+  <div class="absolute bg-white px-2 w-10 text-center
+              @if (is_home()) right-0 @else right-1 @endif
+             ">
     <a
       class="block mt-2 hover-red"
       href="https://www.facebook.com/sharer/sharer.php?u={{ $current_url }}"
