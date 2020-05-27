74920a87d9999b719b84966c924a0c0994d729de
Jure Cuhalev <jure@cuhalev.com>
2020-05-27 16:13:37 +0200

    Update footer for online conference

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,29 +6,24 @@
     >
       <div class="my-2 px-4 md:w-1/4 -mt-4">
         @svg('logo-oeglobal-white', 'max-w-52 md:max-w-full mb-6')
-        @svg('logo-white', 'max-w-52 md:max-w-full')
       </div>
 
-      <div class="my-2 px-4 md:w-1/4">
-        <div class="location-date flex justify-between flex-col
+      <div class="my-2 px-4 flex-grow">
+        <div class="location-date flex justify-center
                     text-white text-sm md:text-lg
                     text-center md:text-left
                     mb-6
                    "
         >
-          <span class="block md:inline mb-4">
-            <span class="block md:inline">@svg('icon-pin')</span>
-            <span class="block md:inline">Taipei Medical University</span>
-          </span>
           <span class="block md:inline">
-            <span class="block md:inline">@svg('icon-calendar')</span>
-            <span class="block md:inline">20-22 November 2020</span>
+            <span class="block md:inline">@svg('icon-calendar', 'inline mr-2 -mt-1')</span>
+            <span class="block md:inline">16-20 November, 2020</span>
           </span>
         </div>
       </div>
 
       <div class="my-2 px-4 md:w-1/2 flex flex-col md:flex-row text-white text-sm md:text-lg sm-only:text-center">
-        <span>@svg('icon-ccby', 'w-24 mt-1 mb-1')</span>
+        <span>@svg('icon-ccby', 'w-24 mt-1 mb-1 inline')</span>
         <p>Except where otherwise noted, content on this site is licensed under a Creative Commons Attribution 4.0 International License.</p>
       </div>
     </div>
